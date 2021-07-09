ad6bcf60413afbc50e7eae55433f44a1e838a3b7
David Gleba <dgleba@gmail.com>
2021-07-09 13:43:30 -0400

    woring.on.watchcopy

--- watchcopy598/watch598.ps1
+++ watchcopy598/watch598.ps1
@@ -1,7 +1,32 @@
 
 # David Gleba # 2021-07-08
-# https://stackoverflow.com/questions/29066742/watch-file-for-changes-and-run-command-with-powershell
 # this tool should only need built in windows 10 tools. No need to install anything.
+
+# orginal post..
+# https://stackoverflow.com/questions/29066742/watch-file-for-changes-and-run-command-with-powershell
+
+# :: notes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# naming the watcher?
+# https://devblogs.microsoft.com/powershell-community/a-reusable-file-system-event-watcher-for-powershell/
+# -SourceIdentifier "watchtest598a" 
+# https://powershell.one/tricks/filesystem/filesystemwatcher
+
+# https://mcpmag.com/articles/2019/05/01/monitor-windows-folder-for-new-files.aspx
+# We can view all existing subscribed events by using the Get-EventSubscriber command. Then, to remove them, use the Unregister-Event cmdlet.
+
+# list events
+# Get-EventSubscriber
+
+# remove all..
+# Get-EventSubscriber | Unregister-Event
+
+
+
+# :: prep ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# run this once..
 # 		mkdir C:\crib\watch598testfolder
 
 
@@ -16,7 +41,7 @@ $watchedfolder = "C:\crib\watch598testfolder"
 Function Register-Watcher {
     param ($folder)
     $filter = "*.*" #all files
-    $watcher = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ 
+    $watcher = New-Object IO.FileSystemWatcher $folder,  $filter -Property @{ 
         IncludeSubdirectories = $false
         EnableRaisingEvents = $true
     }
@@ -32,8 +57,8 @@ Function Register-Watcher {
         & C:\data\script\tools599\watchcopy598\watchcopy598.bat
     ')
 
-    Register-ObjectEvent $Watcher -EventName "Changed" -Action $changeAction
-    Register-ObjectEvent $Watcher -EventName "Created" -Action $changeAction
+    Register-ObjectEvent $Watcher -EventName "Changed" -Action $changeAction -SourceIdentifier "w598changed" 
+    Register-ObjectEvent $Watcher -EventName "Created" -Action $changeAction -SourceIdentifier "w598created" 
 }
 
 
