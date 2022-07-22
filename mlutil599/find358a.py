e33f6058f4d384dac6e6c0a277f11c1e35536021
U-STACKPOLE\dgleba <dgleba@SICS-GZPJL13.stackpole.ca>
2022-07-21 21:29:14 -0400

    tee

---
+++ mlutil599/find358a.py
@@ -0,0 +1,29 @@
+import glob
+print(glob.glob("./*"))
+
+
+"""
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@  
+#@  find358.py using win32 port of unix tee.exe
+#@  
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   2022-07-21[Jul-Thu]17-20PM 
+
+usage:
+
+mkdir \0
+cd \0
+D:\data\script\tools599\mlutil599\find358a.py | D:\data\script\tools599\mlutil599\tee.exe -a out358.txt
+
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@  
+#@  sample output.
+#@  
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   2022-07-21[Jul-Thu]21-23PM 
+
+['.\\00_2. Defect Matrix_M06170 NDT Machine Learning (AI ) Vision System Final_ 13JUN22.xlsx', '.\\88.45.tgr.vnc', '.\\out358.txt', '.\\P76  77_ and 10R140 Gears_defect_matrix_rev04_May 2nd.xlsx', '.\\rsync1.txt', '.\\t', '.\\tarch']
+
+
+"""
